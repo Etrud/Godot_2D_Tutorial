1a906551cdc9e961d177bc7f713138c3868601fb
Etrud <shaffertechnology@gmail.com>
2022-08-29 12:42:08 -0500

    added basic player movement

--- Player.cs
+++ Player.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public class Player : Area2D
 {
@@ -16,12 +15,49 @@ public class Player : Area2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        ScreenSize = GetViewportRect().Size; //get size of viewport 
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        var velocity = Vector2.Zero; // The player's movement vector.
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.x += 1;
+        }
+
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.x -= 1;
+        }
+
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.y += 1;
+        }
+
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.y -= 1;
+        }
+
+        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+            animatedSprite.Play();
+        }
+        else
+        {
+            animatedSprite.Stop();
+        }
+        Position += velocity * delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
+            y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
+            );
+    }
 }
