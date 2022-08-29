175b4a9cb9de3c8a9327a8c5ee3699b2f9337d0f
Etrud <shaffertechnology@gmail.com>
2022-08-29 11:59:48 -0500

    added intial player_node

---
+++ Player.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class Player : Area2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    [Export]
+    public int Speed = 400; // How fast the player will move (pixels/sec).
+
+    public Vector2 ScreenSize; // Size of the game window.
+
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
