bcc1103ac9f42521aca58261f5b8c2651827216e
Etrud <shaffertechnology@gmail.com>
2022-08-29 13:46:06 -0500

    added mobs

--- Player.cs
+++ Player.cs
@@ -1,10 +1,15 @@
 using Godot;
+using static Player;
 
 public class Player : Area2D
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    
+    [Signal]
+    public delegate void hit();
+
 
     [Export]
     public int Speed = 400; // How fast the player will move (pixels/sec).
@@ -15,7 +20,25 @@ public class Player : Area2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        Hide();
         ScreenSize = GetViewportRect().Size; //get size of viewport 
+    }
+
+    //Signals
+    public void OnPlayerBodyEntered(PhysicsBody2D body)
+    {
+        Hide(); // Player disappears after being hit.
+        EmitSignal(nameof(hit));
+        // Must be deferred as we can't change physics properties on a physics callback.
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
+    }
+
+    //Functions
+    public void Start(Vector2 pos)
+    {
+        Position = pos;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
     }
 
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -54,10 +77,26 @@ public class Player : Area2D
         {
             animatedSprite.Stop();
         }
+
+        //Clamp onto screen
         Position += velocity * delta;
         Position = new Vector2(
             x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
             y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
             );
+
+        //Choosing animations
+        if (velocity.x != 0)
+        {
+            animatedSprite.Animation = "walk";
+            animatedSprite.FlipV = false;
+            // See the note below about boolean assignment.
+            animatedSprite.FlipH = velocity.x < 0;
+        }
+        else if (velocity.y != 0)
+        {
+            animatedSprite.Animation = "up";
+            animatedSprite.FlipV = velocity.y > 0;
+        }
     }
 }
